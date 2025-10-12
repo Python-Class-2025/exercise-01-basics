3c241e83670b530c22963fd24f09194c782f077e
mrxacker <49713930+mrxacker@users.noreply.github.com>
2025-10-12 19:57:49 +0500

    Create ex01_print.py
    initial commit

---
+++ exercises/ex01_print.py
@@ -0,0 +1,14 @@
+"""
+Exercise 01: Basic Printing and Variables
+-----------------------------------------
+
+Instructions:
+- Write code below each TODO comment.
+- Run your code locally to test before making a Pull Request.
+"""
+
+# TODO: Print "Hello, Python!"
+
+# TODO: Create a variable 'name' with your full name and print "My name is <name>"
+
+# TODO: Create two numbers, add them together and print the result
