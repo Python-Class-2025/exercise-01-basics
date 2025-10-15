179a679c66974d5e6cf9aeef0ce5a0744cee977f
mrxacker <orazorazow1998@gmail.com>
2025-10-15 16:26:57 +0500

    .

--- exercises/ex01_print.py
+++ exercises/ex01_print.py
@@ -11,6 +11,9 @@ Instructions:
 
 print('Hello World')
 
+
+
+
 # TODO: Create a variable 'name' with your full name and print "My name is <name>"
 
 # TODO: Create two numbers, add them together and print the result
